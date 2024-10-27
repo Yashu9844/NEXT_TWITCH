6b6807606cabfe7924a23e9cba10b8f2b813b83c
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-10-27 14:52:19 +0530

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+  return (
+    <div className="bg-white h-screen w-full " >
+      <Button>Page</Button>
+    </div>
+  );
+};
+
+export default Page;
