ebe906f6d86560549887dc20f7becf55cd1f40d9
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-11-02 14:58:12 +0530

    add function to get alll followers

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -130,3 +130,26 @@ where:{
         throw new Error("Something went wrong")
     }
 }
+
+
+export const getFollowedUsers = async()=>{
+    try {
+        
+      const self = await getSelf();
+
+      const followedUsers = await db.follow.findMany({
+        where:{
+            followerId:self.id
+        },include:{
+            following:true,
+            
+        }
+      })
+ 
+
+      return followedUsers;
+
+    } catch (error) {
+        throw new Error("Something went wrong")
+    }
+}
