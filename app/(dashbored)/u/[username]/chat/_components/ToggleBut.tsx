be12748c4ddc17d0ff4002a7ebffe01aa5dc18ff
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-11-06 22:25:58 +0530

    commit

--- app/(dashbored)/u/[username]/chat/_components/ToggleBut.tsx
+++ app/(dashbored)/u/[username]/chat/_components/ToggleBut.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Switch } from "@/components/ui/switch";
 
 type FieldType = "isChatEnabled" | "isChatDelayed" | "isChatFollowersOnly"
--- app/(dashbored)/u/[username]/chat/page.tsx
+++ app/(dashbored)/u/[username]/chat/page.tsx
@@ -1,12 +1,13 @@
 import { getSelf } from "@/lib/auth-service";
 import { getUserStreamIdByuserId } from "@/lib/stream-service";
 import ToggleBut from "./_components/ToggleBut";
+import { notFound } from "next/navigation";
 
 const ChatPage = async () => {
  
     const self = await getSelf();
     const stream = await getUserStreamIdByuserId(self.id)
-
+ if(!stream) return notFound();
   return (
     <div className="p-6">
       <div className="mb-4">
