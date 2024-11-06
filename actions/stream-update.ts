21a502c1bbc569715c69b86ca0626d9c25af04dc
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-11-06 22:42:58 +0530

    add updatestream action

---
+++ actions/stream-update.ts
@@ -0,0 +1,52 @@
+
+"use server"
+
+import { getSelf } from "@/lib/auth-service"
+import { db } from "@/lib/db"
+import { getUserStreamIdByuserId } from "@/lib/stream-service"
+
+
+import {Stream} from '@prisma/client'
+import { revalidatePath } from "next/cache"
+
+
+export const  updateStream = async (values:Partial<Stream>)=>{
+    try {
+
+        const self = await getSelf()
+        const selfStream = await db.stream.findUnique({
+            where:{
+                userId:self.id
+            }
+        })
+
+        if(!selfStream){
+            throw new Error("User does not have a stream")
+
+        }
+       const validData ={
+        name: values.name ,
+    isChatEnabled: values.isChatEnabled ,
+    isChatDelayed: values.isChatDelayed ,
+    isChatFollowersOnly: values.isChatFollowersOnly ,
+       }
+
+            const stream = await db.stream.update({
+                where:{
+                    userId: selfStream.id
+                },
+                data:{
+                    ...validData
+                }
+            })
+
+            revalidatePath(`/u/${self.username}/chat`)
+            revalidatePath(`/u/${self.username}`)
+            revalidatePath(`/${self.username}`)
+
+  return stream
+        
+    } catch (error) {
+        throw new Error("internal server error: " )
+    }
+}
