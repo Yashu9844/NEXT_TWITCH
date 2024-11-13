121aac481edfd4792304283ea89f8b754a831d27
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-11-14 01:53:55 +0530

    add  chat header

--- components/StreamPlayer.tsx
+++ components/StreamPlayer.tsx
@@ -2,8 +2,11 @@
 
 import { useViewerToken } from "@/hooks/UserViewerToken";
 import { Stream, User } from "@prisma/client";
-import { LiveKitRoom} from '@livekit/components-react'
+import {  LiveKitRoom} from '@livekit/components-react'
 import Video from "./stream-player/Video";
+import { useChatSideBar } from "@/store/use-chat-sidebar";
+import { cn } from "@/lib/utils";
+import Chat from "./stream-player/Chat";
 
 interface StreamPlayerProps{
     user:User & {
@@ -17,6 +20,7 @@ interface StreamPlayerProps{
 
 const StreamPlayer = ({user,stream,isFollowing} : StreamPlayerProps) => {
   const {token , name,identity} = useViewerToken(user.id)
+  const {collapsed} = useChatSideBar((state)=>state)
  console.log(
   
    "identity: " + identity,
@@ -34,7 +38,10 @@ const StreamPlayer = ({user,stream,isFollowing} : StreamPlayerProps) => {
     <LiveKitRoom 
     token={token}
     serverUrl={process.env.NEXT_PUBLIC_WS_LIVEKIT_URL}
-    className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+    className={cn("grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+      collapsed &&  "lg:grid-cols-2  xl:grid-cols-2"
+    )
+    }
     >
       <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
         <Video
@@ -43,6 +50,17 @@ const StreamPlayer = ({user,stream,isFollowing} : StreamPlayerProps) => {
         
         />
         
+      </div>
+      <div className={cn("col-span-1" , collapsed && "hidden")}>
+        <Chat
+        viewerName = {name}
+        hostName = {user.username}
+        hostIdentity = {user.id}
+        isFollowing = {isFollowing}
+        isChatEnabeled = {stream.isChatEnabled}
+        isChatDelayed = {stream.isChatDelayed}
+        isChatFollowersOnly = {stream.isChatFollowersOnly}
+        />
       </div>
     </LiveKitRoom>
   );
---
+++ components/stream-player/ChatHeader.tsx
@@ -0,0 +1,22 @@
+import { Skeleton } from "../ui/skeleton";
+
+const ChatHeader = () => {
+  return (
+    <div className="p-3 border-b realtive">
+      <div className="font-semibold text-primary text-center">
+    Stream Chat
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
+
+export const ChatHeaderSkeleton = ()=>{
+    return (
+        <div className="relative p-3 border-b hidden md:block">
+            <Skeleton className="absolute h-6 w-6 left-3 top-3" />
+            <Skeleton className="w-28 h-6 mx-auto" />
+        </div>
+    )
+}
