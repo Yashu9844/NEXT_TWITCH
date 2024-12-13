73d7e04db4d2e8ba45540c73a63749dbe0536952
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-12-14 03:28:59 +0530

    design the dashbored with filter and sorting for blocked users

--- app/(dashbored)/u/[username]/community/_components/columns.tsx
+++ app/(dashbored)/u/[username]/community/_components/columns.tsx
@@ -3,6 +3,7 @@
 import UserAvatar from "@/app/(browser)/_components/sidebar/UserAvatar"
 import { Button } from "@/components/ui/button"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -15,23 +16,39 @@ export type Blockedusers = {
 }
 
 export const columns: ColumnDef<Blockedusers>[] = [
-  {
-    accessorKey: "username",
-    header: "Username",
-    cell:({row})=>(
-        <div className="flex items-center gap-x-4">
+    {
+        accessorKey: "username",
+        header: ({ column }) => (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Username
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        ),
+        cell: ({ row }) => (
+          <div className="flex items-center gap-x-4">
             <UserAvatar
-            username={row.original.username}
-            imageUrl={row.original.imageUrl}
+              username={row.original.username}
+              imageUrl={row.original.imageUrl}
             />
             <span>{row.original.username}</span>
-        </div>
-    )
-  },
-  {
-    accessorKey: "createdAt",
-    header: "Date Blocked",
-  },
+          </div>
+        ),
+      },
+      {
+        accessorKey: "createdAt",
+        header: ({ column }) => (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Date blocked
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        ),
+      },
   {
     id: "actions",
    cell:()=><Button>Unblock</Button>
